59ecef5155173127b51662db404177606fb55c8e
Victor Tarasenko <vtarasenko@softjourn.com>
2014-10-17 09:56:00 +0300

    Commit 1

---
+++ RenderingHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace ElkRiv.Web.PrivateLabel.Code.Helpers
+{
+    public static class RenderingHelper
+    {
+        public static string RenderViewToString(ControllerContext context, string viewName, object model)
+        {
+            if (String.IsNullOrEmpty(viewName))
+                viewName = context.RouteData.GetRequiredString("action");
+
+            var viewData = new ViewDataDictionary(model);
+
+            using (var sw = new StringWriter())
+            {
+                //Get the view and setup the context
+                var viewResult = ViewEngines.Engines.FindPartialView(context, viewName);
+                var viewContext = new ViewContext(context, viewResult.View, viewData, new TempDataDictionary(), sw);
+
+                viewContext.ViewBag.StoreFront = context.Controller.ViewBag.StoreFront;
+
+                //Copy the current model state
+                foreach (var item in context.Controller.ViewData.ModelState)
+                {
+                    viewContext.ViewData.ModelState.Add(item);
+                }
+
+                //Render the view to a string
+                viewResult.View.Render(viewContext, sw);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
